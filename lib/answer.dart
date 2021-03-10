20a8d289576530f5675b67168d33f7765353b79b
Ateeq <ateeg1999@gmail.com>
2021-03-10 12:54:51 +0200

    add quiz app widgets files and pass data to other widget from th main file

---
+++ lib/answer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final String answerText;
+  final Function selectHandler;
+
+  Answer(this.answerText, this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      // alignment: Alignment.center,
+      child: ElevatedButton(
+        child: Text(answerText),
+        onPressed: selectHandler(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,59 @@
+import 'package:first_app/quiz.dart';
 import 'package:flutter/material.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'what\'s your favorite color?',
+      'answers': ['White', 'Black', 'Green', 'Blue']
+    },
+    {
+      'questionText': 'what\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephent', 'Lion']
+    },
+    {
+      'questionText': 'who\'s your favorite instructor?',
+      'answers': ['Max', 'Ateeq', 'Aymen', 'Zamo']
+    },
+  ];
+
+  var _questionIndex = 0;
+
+  void _answerQuestions() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'what\'s your favorite color?',
-      'what\'s your favorite animal?'
-    ];
-
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My AppBar Title'),
-          ),
-          body: Column(
-            children: [
-              Text('The Question'),
-              ElevatedButton(
-                child: Text('Answer 1'),
-                onPressed: null,
-              ),
-              ElevatedButton(
-                child: Text('Answer 2'),
-                onPressed: null,
-              ),
-              ElevatedButton(
-                child: Text('Answer 3'),
-                onPressed: null,
-              ),
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('My AppBar Title'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                _questions,
+                _questionIndex,
+                _answerQuestions,
+              )
+            : Result(),
+      ),
     );
   }
 }
